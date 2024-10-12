baf54975b2150b15e77fc2fe42e0a3f89c8910ae
James Cox <james@imaj.es>
2024-10-11 19:37:27 -0500

    First import

---
+++ lib/pivotal_migrator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "pivotal_migrator/version"
+
+module PivotalMigrator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pivotal_migrator/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module PivotalMigrator
+  VERSION = "0.1.0"
+end
